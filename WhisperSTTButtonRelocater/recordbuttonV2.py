f08ed31c4231c3a6d4ce009cbd65de1d133660e5
RandoInternetPreson <aaronalai1@gmail.com>
2023-12-31 16:54:16 -0500

    Add files via upload
    changes color and text when recording

---
+++ WhisperSTTButtonRelocater/recordbuttonV2.py
@@ -0,0 +1,36 @@
+def create_ui():
+    mu = shared.args.multi_user
+
+    # ... [rest of the existing code prior to the generate-stop-container] ...
+
+    with gr.Row(elem_id='generate-stop-container'):
+        with gr.Column():
+            shared.gradio['Stop'] = gr.Button('Stop', elem_id='stop', visible=False)
+            shared.gradio['Generate'] = gr.Button('Generate', elem_id='Generate', variant='primary')
+            # New "RecD" button
+            shared.gradio['RecD'] = gr.Button('RecD', elem_id='RecD')
+
+    # JavaScript action for the "RecD" button to toggle text and color
+    shared.gradio['RecD'].click(None, None, None, _js="""() => {
+        const recDButton = document.getElementById('RecD');
+        const isRecording = recDButton.textContent === 'Active';
+        Array.from(document.querySelectorAll('button')).find(
+            button => button.textContent.includes('Record from microphone') || button.textContent.includes('Stop recording')
+        ).click();
+        
+        if (isRecording) {
+            recDButton.textContent = 'RecD';
+            recDButton.style.color = ''; // Default color
+        } else {
+            recDButton.textContent = 'Active';
+            recDButton.style.color = 'red'; // Active color
+        }
+    }""")
+
+    # ... [rest of the existing code after the generate-stop-container] ...
+
+    # Define all other elements, interactions, and layout as per your original code
+    # ...
+
+# Don't forget to call create_ui() to build the interface
+create_ui()
