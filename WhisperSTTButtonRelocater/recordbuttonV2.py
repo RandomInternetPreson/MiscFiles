31d05b9c82b6bb750773e51092308bb6d0b80627
RandoInternetPreson <aaronalai1@gmail.com>
2023-12-31 17:04:50 -0500

    Update recordbuttonV2.py

--- WhisperSTTButtonRelocater/recordbuttonV2.py
+++ WhisperSTTButtonRelocater/recordbuttonV2.py
@@ -3,7 +3,7 @@ def create_ui():
 
     # ... [rest of the existing code prior to the generate-stop-container] ...
 
-    with gr.Row(elem_id='generate-stop-container'):
+    with gr.Column(elem_id='generate-stop-container'):
         with gr.Row():
             shared.gradio['Stop'] = gr.Button('Stop', elem_id='stop', visible=False)
             shared.gradio['Generate'] = gr.Button('Generate', elem_id='Generate', variant='primary')
